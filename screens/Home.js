20349602b06980a10915fffeae70e67c19a22e5f
nishank95 <nishanksamant@gmail.com>
2020-12-28 12:07:19 +1100

    adding dummy data and rendering header

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,91 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+
+import {icons, images, COLORS, SIZES, FONTS} from '../constants';
+import {initialCurrentLocation, categoryData, restaurantData} from '../data';
 
 const Home = () => {
-  return (
-    <View>
-      <Text> Home </Text>
-    </View>
+  const [categories, setCategories] = useState(categoryData);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [restaurants, setRestaurants] = useState(restaurantData);
+  const [currentLocation, setCurrentLocation] = useState(
+    initialCurrentLocation,
   );
+
+  function renderHeader() {
+    return (
+      <View style={{flexDirection: 'row', height: 50}}>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingLeft: SIZES.padding * 2,
+            justifyContent: 'center',
+          }}>
+          <Image
+            source={icons.nearby}
+            style={{
+              width: 30,
+              height: 30,
+            }}
+          />
+        </TouchableOpacity>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <View 
+            style={{
+              backgroundColor: COLORS.lightGray3,
+              borderRadius: SIZES.radius,
+              width: '70%',
+              height: '100%',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text style={{...FONTS.h4}}>{currentLocation.streetName}</Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingRight: SIZES.padding * 2,
+            justifyContent: 'center',
+          }}>
+          <Image
+            source={icons.basket}
+            style={{
+              width: 30,
+              height: 30,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  return <SafeAreaView style={styles.container}>{renderHeader()}</SafeAreaView>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray4,
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 1,
+  },
+});
 
 export default Home;
